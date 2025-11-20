bb6c5da2fdf74f253aeefa503b0e8c4aa4b043e2
Waniyah950 <itsWani.sWAAAAN@gmail.com>
2025-11-20 22:55:28 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        roseblood: '#B91C1C',
-        blush: '#FDF6EC',
-        charcoal: '#1F1F1F',
+        roseblood: '#800020',
       },
     },
   },
